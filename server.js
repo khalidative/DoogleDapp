3fa3b8fac6b616615fd5aa6c7a2408af52d58039
Waleed Mohamed <gmwaleed27@gmail.com>
2019-03-20 19:57:17 +0530

    changed server

--- server.js
+++ server.js
@@ -25,4 +25,4 @@ app.post('/pusher/auth', function(req, res) {
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Listening at http://localhost:5000'));
+app.listen(port, () => console.log('Listening at http://localhost:' + port));
